1270a4181fadb87abd2f4ddb6c5b7e7cd502458e
psilonpneuma <chiara.semenzin@gmail.com>
2020-04-07 16:27:58 +0200

    Update config.py

--- create_subjects/config.py
+++ create_subjects/config.py
@@ -2,10 +2,10 @@
 
 config = {
 	"python":"python",
-	"working_dir": "/scratch2/csemenzin/lena_elisa/", # working directory
+	"working_dir": "/scratch2/csemenzin/lena_elisa/", # folder with its and wav files
     "scripts":"/scratch2/csemenzin/lena_elisa/scripts/", #dir with scripts
     "praat": "praat", # path to praat exe
-    "infolder": "/scratch2/csemenzin/lena_elisa/output/", # folder with its and wav files
-    "outfolder": "/scratch2/csemenzin/lena_elisa/output/extracts/", # folder to extract output
+    "infolder": "/scratch2/csemenzin/lena_elisa/output/", # folder to extract first segmentation
+    "outfolder": "/scratch2/csemenzin/lena_elisa/output/extracts/", # folder to extract final clips
     "metadata_fn":"praat_metadata.txt", # default; not yet implemented - change filename in praat script
 }
