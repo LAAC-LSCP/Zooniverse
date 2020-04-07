b0c51b3e39fb57d7856bd9cadfaacddfb81ca8ea
psilonpneuma <chiara.semenzin@gmail.com>
2020-04-07 17:30:15 +0200

    Update pipeline.py

--- create_subjects/pipeline.py
+++ create_subjects/pipeline.py
@@ -38,11 +38,11 @@ with open(outfolder+'metadata_praat.txt', 'r') as file:
 	data = file.readlines()
 	for i,line in enumerate(data):
 		line_new=line.split("\t")
-		key = line_new[1].split("_")[0]
-		age = line_new[1].split("_")[1]
-		its = line_new[1].split("_")[2:6]
+		key = line_new[1].split("_")[1]
+		age = line_new[1].split("_")[2]
+		its = "_".join(line_new[1].split("_")[3:6])
 		data[i] = line_new[0]+"\t{}\t{}\t{}\n".format(key, age, its)
-		with open('metadata_praat.txt', 'w') as f:
+		with open('metadata_praat_new.txt', 'w') as f:
 			f.writelines(data)
-print("\nDone.")
+print("Done.")
 #TODO: add gender
