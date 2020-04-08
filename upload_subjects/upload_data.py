4fc1ee5a7e6ab4f0c187220f95f1cf9e5cd13445
psilonpneuma <chiara.semenzin@gmail.com>
2020-04-08 11:21:37 +0200

    Update upload_data.py

--- upload_subjects/upload_data.py
+++ upload_subjects/upload_data.py
@@ -1,16 +1,16 @@
 # activate virtualenv
 import os
 
-extracts_dir="/scratch2/csemenzin/lena_files/output/extracts/"
-batch_name="default"
+extracts_dir="/scratch2/csemenzin/lena_files/output/extracts/" #where the clips are
+batch_name="default" #change this to upload batch name to use 
 
 os.system("cd {}".format(extracts_dir))
 os.system("printf "Name,Type\n" >> manifest.csv;")
-os.system("for i in *.mp3; do printf "$i,new_lenas\n" >> manifest.csv ; done")
+os.system("for i in *.mp3; do printf "$i,lenas\n" >> manifest.csv ; done")
 
 print("Create new subject set")
 os.system("panoptes subject-set create 10073 "+ batch_name))
-subj_set = raw_input("What is the subject set number?")
+subj_set = raw_input("What is the subject set number?") 
 os.system("panoptes subject-set upload-subjects "+subj_set+ "manifest.csv")
 
 print("uploading subjects")
