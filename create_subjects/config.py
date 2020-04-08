c27f1ae04558a13eff3990d29cf8e4618388f8c0
psilonpneuma <chiara.semenzin@gmail.com>
2020-04-08 11:09:53 +0200

    Update config.py

--- create_subjects/config.py
+++ create_subjects/config.py
@@ -3,11 +3,11 @@
 
 config = {
 	"python":"python",
-	"working_dir": "/Users/chiarasemenzin/Desktop/create_data/", # working directory
-    "scripts":"/Users/chiarasemenzin/Desktop/create_data/", 
-    "praat": "/Applications/Praat.app/Contents/MacOS/Praat", # path to praat exe
-    "infolder": "/Users/chiarasemenzin/Desktop/create_data/sample_data/", # folder with its and wav files
-    "outfolder": "/Users/chiarasemenzin/Desktop/create_data/sample_data/output/extracts/", # folder to extract output
-    "metadata_fn":"MetaData_prova.txt", # metadata filename
-    "make_folders":False
+	"working_dir": "/Users/xxx/Documents/gitrepos/Zooniverse/create_data/", # working directory
+	"scripts":"/Users/xxx/Documents/gitrepos/Zooniverse/create_data/", 
+	"praat": "/Applications/Praat.app/Contents/MacOS/Praat", # path to praat exe
+	"infolder": "/Users/xxx/Documents/gitrepos/Zooniverse/create_data/sample_data/", # folder with its and wav files
+	"outfolder": "/Users/xxx/Documents/gitrepos/Zooniverse/create_data/sample_data/output/extracts/", # folder to extract output	
 }
+
+#todo: md_fn, mk_fldrs
