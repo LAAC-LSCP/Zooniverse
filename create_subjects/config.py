d51e89cf1da9401c9a064a0c3a75078396ed09c0
psilonpneuma <chiara.semenzin@gmail.com>
2020-04-13 17:57:56 +0200

    Update config.py

--- create_subjects/config.py
+++ create_subjects/config.py
@@ -3,11 +3,9 @@
 
 config = {
 	"python":"python",
-	"working_dir": "/Users/xxx/Documents/gitrepos/Zooniverse/create_data/", # working directory
-	"scripts":"/Users/xxx/Documents/gitrepos/Zooniverse/create_data/", 
-	"praat": "/Applications/Praat.app/Contents/MacOS/Praat", # path to praat exe
-	"infolder": "/Users/xxx/Documents/gitrepos/Zooniverse/create_data/sample_data/", # folder with its and wav files
-	"outfolder": "/Users/xxx/Documents/gitrepos/Zooniverse/create_data/sample_data/output/extracts/", # folder to extract output	
+	"working_dir": "/Users/chiarasemenzin/Desktop/create_temp/", # working directory
+    "infolder": "/Users/chiarasemenzin/Desktop/create_temp/sample_data/", # folder with its and wav files
+    "outfolder": "/Users/chiarasemenzin/Desktop/create_temp/sample_data/output/extracts/", # folder to extract output
+    "metadata_fn":"metadataaaaa.txt", # metadata filename
 }
 
-#todo: md_fn, mk_fldrs
