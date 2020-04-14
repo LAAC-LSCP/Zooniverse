99a8700f8e485c05d2dfa2e60b8354acbed99ab7
psilonpneuma <chiara.semenzin@gmail.com>
2020-04-14 11:35:14 +0200

    Update config.py

--- create_subjects/config.py
+++ create_subjects/config.py
@@ -3,9 +3,9 @@
 
 config = {
 	"python":"python",
-	"working_dir": "/Users/chiarasemenzin/Desktop/create_temp/", # working directory
-    "infolder": "/Users/chiarasemenzin/Desktop/create_temp/sample_data/", # folder with its and wav files
-    "outfolder": "/Users/chiarasemenzin/Desktop/create_temp/sample_data/output/extracts/", # folder to extract output
-    "metadata_fn":"metadataaaaa.txt", # metadata filename
+	"working_dir": "/Users/xxx/Zooniverse-master/create_subjects/", # working directory
+    "infolder": "/Users/xxx/Zooniverse-master/create_subjects/sample_data/", # folder with its and wav files
+    "outfolder": "/Users/xxx/Zooniverse-master/create_subjects/sample_data/output/extracts/", # folder to extract output
+    "metadata_fn":"metadata_file.txt", # metadata filename
 }
 
