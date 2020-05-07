cac2d44a5ce4d04b5226bda99f506b68818cf96f
psilonpneuma <chiara.semenzin@gmail.com>
2020-05-07 15:47:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@
 python = "python"
 #leave it as python if your only python version is python3; or use python3 if you have several so that python3 is used
 
-infolder = "/Users/chiarasemenzin/Desktop/create_temp/sample_data/"
+infolder = "/Users/example/Desktop/sample_data/"
 # folder with your source its and wav files
 
-outfolder =  "/Users/chiarasemenzin/Documents/Zooniverse-data/"
+outfolder =  "/Users/example/Documents/Zooniverse-data/"
 # folder where output will be saved. If it does not exist, it will be created in the process.
-# this will create two folders: 
+# this will create two sub-folders: 
 # - an intermediate/ folder with extracted CHN chunks
 # - a data-for-upload/ folder with the final short clips
 
@@ -17,6 +17,8 @@ dataset_name = "LAAC_20200418_ac1"
 # metadata filename, choose anything you'd like since it will be created in the process
 
 batch_name = "LAAC_20200418_ac1"
-#name that will be used to label the batch during uploading, choose anything you want as long as there are no spaces and it is unique (i.e., it will not have been used by you or any others in the past). NOTE! You cannot use the same name twice (i.e., for two subsequent uploads).
+#name that will be used to label the batch during uploading, choose anything you want as long as there are no spaces 
+#and it is unique (i.e., it will not have been used by you or any others in the past). 
+#NOTE! You cannot use the same name twice (i.e., for two subsequent uploads).
 
 
