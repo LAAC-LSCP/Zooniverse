df05c745e4c554fc6a39c21363ad3c03e88e5c24
psilonpneuma <chiara.semenzin@gmail.com>
2020-05-12 13:39:24 +0200

    Fixed bug in manifest name

--- upload_subjects/upload_data.py
+++ upload_subjects/upload_data.py
@@ -17,13 +17,14 @@ for batch in extracts_batches:
     os.chdir(batch)
     batch_name=os.path.basename(os.path.normpath(batch))
     print("Processing {}".format(batch_name))
-    os.system("printf \"Name,Type\n\" > manifest.csv;")
-    os.system("for i in *.mp3; do printf \"$i,lenas\n\" >> manifest.csv ; done")
+    cmd="printf \"Name,Type\n\" > manifest_"+batch_name+".csv;"
+    os.system(cmd)
+    os.system("for i in *.mp3; do printf \"$i,lenas\n\" >> manifest_"+batch_name+".csv ; done")
     print("Creating a new subject set")
     subj_set=subprocess.getoutput("panoptes subject-set create 10073 "+batch_name).split(" ")[0]
     if subj_set == "Traceback":
         print("ERROR! Batch name already exists. Please choose a different name of remove the existing one from Zooniverse.")
         continue
-    os.system("panoptes subject-set upload-subjects "+subj_set+ " manifest.csv")
+    os.system("panoptes subject-set upload-subjects "+subj_set+ " manifest_"+batch_name+".csv")
 
     print("Uploading subjects")
