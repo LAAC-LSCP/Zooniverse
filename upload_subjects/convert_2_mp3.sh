f056235d40fb98e977fce726ce5e4390dbc88edd
psilonpneuma <chiara.semenzin@gmail.com>
2020-05-18 13:10:59 +0200

    Fixed loop bug

--- upload_subjects/convert_2_mp3.sh
+++ upload_subjects/convert_2_mp3.sh
@@ -1,16 +1,15 @@
 outfolder=`grep "outfolder" ../config.py | awk '{ gsub(".*=", "") ; system( "echo "  $0) }'`
 
 echo Converting WAV files to MP3 using SOX...
-
 #CONVERT TO MP3
 
-for filename in $outfolder/data-for-upload/*.wav;
+for filename in $outfolder/*for_upload/*batch*/*.wav;
 do echo "${filename%.*}";
 sox "${filename%.*}".wav "${filename%.*}".mp3;
 done
 
 #REMOVE WAVs
 
-for filename in $outfolder/data-for-upload/*.wav;
-do rm $filename;
-done
+#for filename in $outfolder/data-for-upload/*.wav;
+#do rm $filename;
+#done
